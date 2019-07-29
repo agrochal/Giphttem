9ba347b71133abf26ea953e56484cdd94a0163c7
Artur Grochal <artur@procer.pl>
2019-07-29 20:19:22 +0200

    Giphttem v1.0.0 ❤️

---
+++ app.js
@@ -0,0 +1,58 @@
+const fetch = require('node-fetch');
+const request = require('request');
+const config = require('./config');
+const fs = require('fs');
+const Twit = require('twit');
+
+let jsondata;
+var T = new Twit(config.keys);
+
+function giphy() {
+   fetch("http://api.giphy.com/v1/gifs/random?&api_key=" + config.api)
+      .then(function(u) {
+         return u.json();
+      }).then(
+         function(json) {
+            jsondata = json;
+            download(jsondata.data.image_original_url, 'random.gif')
+         }
+      )
+}
+
+
+
+function download(url, filename) {
+   request.head(url, downloading);
+
+   function downloading(err, res, body) {
+      request(url).pipe(fs.createWriteStream(filename)).on('close', finished);
+   }
+
+   function finished() {
+      var b64content = fs.readFileSync('random.gif', {
+         encoding: 'base64'
+      });
+      T.post('media/upload', {
+         media_data: b64content
+      }, uploaded);
+   }
+}
+
+function uploaded(err, data, response) {
+   var mediaIdStr = data.media_id_string;
+   var params = {
+      status: jsondata.data.title,
+      media_ids: [mediaIdStr]
+   }
+   T.post('statuses/update', params, tweeted);
+};
+
+function tweeted(err, data, response) {
+   if (err) {
+      console.log(err);
+   } else {
+      console.log('Success: ' + data.text);
+   }
+};
+
+giphy();
---
+++ configTMP.js
@@ -0,0 +1,18 @@
+/*
+   Type your keys here and rename file to config.js
+*/
+
+/* Twitter keys */
+module.exports.keys = {
+   consumer_key:         '...',
+   consumer_secret:      '...',
+   access_token:         '...',
+   access_token_secret:  '...',
+}
+
+/*
+   Type here your NASA API key
+   You can find it here: https://developers.giphy.com/
+*/
+
+module.exports.api = "...";
